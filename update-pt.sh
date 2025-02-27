7f00b96ca35eff02b3e1adea66a21841418493a0
syphyr <syphyr@gmail.com>
2025-02-27 03:17:10 +0100

    Add script to update snowflake and lyrebird modules

---
+++ update-pt.sh
@@ -0,0 +1,8 @@
+cd IPtProxy.go
+
+go get gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/goptlib@latest
+go get gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/lyrebird@latest
+go get gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/snowflake/v2@latest
+go get golang.org/x/net@latest
+go get golang.org/x/mobile/cmd/gomobile@latest
+go mod tidy
